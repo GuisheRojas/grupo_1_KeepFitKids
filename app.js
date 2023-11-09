fdf733ae0868ac6a266f5ce4a6394cbeaa93a675
AnitaMigueles <anita.migueles@gmail.com>
2023-11-09 03:09:00 -0300

    Terminé el home, header y footer

--- app.js
+++ app.js
@@ -19,3 +19,4 @@ app.get('/login', (req, res)=>res.sendFile(__dirname + "/views/html/login.html")
 app.get('/productCart', (req, res)=>res.sendFile(__dirname + "/views/html/productCart.html"))
 
 app.get('/register', (req, res)=>res.sendFile(__dirname + "/views/html/register.html"))
+
