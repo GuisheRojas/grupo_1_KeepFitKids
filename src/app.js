36e0cd3072cdfda2a571449ed6ead66304476ee6
AnitaMigueles <anita.migueles@gmail.com>
2023-12-05 07:27:22 -0300

    Hice lo referido a method-override

--- src/app.js
+++ src/app.js
@@ -7,12 +7,14 @@ const mainRouter = require('./routes/main');
 const productRouter = require('./routes/products');
 const userRouter = require('./routes/user');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', path.join(__dirname, 'views'));
+app.use(methodOverride('_method'));
 
 app.listen(PORT, ()=>{
     console.log(`Servidor iniciado en http://localhost:${PORT}`)
