434820fa3b5f26c7fe183b5be642a6dd5cf58a12
AnitaMigueles <anita.migueles@gmail.com>
2024-03-16 20:49:17 -0300

    Hice la api de productos

--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -4,38 +4,58 @@ const {Op} = require('sequelize');
 
 const productsApiController = {
     list: async (req, res) => {
-        const products = await db.Products.findAll()
+        const productos = await db.Products.findAll({
+            attributes:['id', 'name', 'description'],
+            include: [{model: db.Stock, as: 'products_stock', attributes: ['quantity'],
+                include: [
+                    {model: db.Colors,as: 'colors_stock', attributes: ['name']}, 
+                    {model: db.Sizes, as: 'sizes_stock', attributes: ['name']}
+                ]
+            }]
+        });
+        let contFem = 0;
+        let contMasc = 0;
+        let contUni = 0;
+        for (let i = 0; i < productos.length; i++) {
+            const element = productos[i];
+            element.dataValues.detail = 'http://localhost:8000' + req.originalUrl + '/' + element.id;
+            if(element.category == 'Femenino'){
+                contFem ++;
+            } else if(element.category == 'Masculino'){
+                contMasc ++;
+            } else{
+                contUni ++;
+            }
+        }
         let respuesta = {
-            meta: {
-                status: 200,
-                total: products.length,
-                url: req.params.originalUrl
+            count: productos.length,
+            countByCategrory: {
+                Femenino: contFem,
+                Masculino: contMasc,
+                Unisex: contUni
             },
-            data: { ...products }
+            products: { ...productos }
         }
         res.json(respuesta)
     },
 
     detail: async (req, res) => {
-        const product = await db.Products.findByPk(req.params.id)
-        let respuesta = {
-            meta: {
-                status: 200,
-                total: product.length,
-                url: req.params.originalUrl
-            },
-            data: { ...product }
-        }
-        res.json(respuesta)
+        const product = await db.Products.findByPk(req.params.id, {
+            include: [{model: db.Stock, as: 'products_stock', attributes: ['quantity'],
+                include: [
+                    {model: db.Colors,as: 'colors_stock', attributes: ['name']}, 
+                    {model: db.Sizes, as: 'sizes_stock', attributes: ['name']}
+                ]
+            }]
+        })
+        res.json(product)
     },
 
-    create: (req, res) => {
-        
-    },
+    create: async (req, res) => {},
 
     update: (req, res) => {},
 
-    destroy: (req, res) => {},
+    destroy: async (req, res) => {}
 }
 
 module.exports = productsApiController;
