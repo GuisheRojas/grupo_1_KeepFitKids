b5b32fb097912e1618752585fb77dc6e953b1d85
AnitaMigueles <anita.migueles@gmail.com>
2024-03-16 20:58:13 -0300

    Creé las rutas de la api de usuarios

--- src/routes/api/usersApiRoutes.js
+++ src/routes/api/usersApiRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
                             
 const usersApiController = require('../../controllers/api/usersApiController');
 
+router.get('/', usersApiController.list)
+
+router.get('/:id', usersApiController.detail)
 
 module.exports = router;
