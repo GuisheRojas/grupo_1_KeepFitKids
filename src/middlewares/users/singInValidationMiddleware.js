199cbd215ad4f1bee513d0970900060d6fe2c7dd
Santiago-Rearte <santyrearte@gmail.com>
2024-03-17 23:36:49 -0300

    Rehaciendo el middleware del signInvalidation

--- src/middlewares/users/singInValidationMiddleware.js
+++ src/middlewares/users/singInValidationMiddleware.js
@@ -1,11 +1,62 @@
-const {check} = require('express-validator')
 
-let singInValidation = [
-    check('email')
-        .notEmpty().withMessage('Debe ingresar un email')
-        .isEmail().withMessage('Formato de email invalido'),
-    check('password')
-        .notEmpty().withMessage('Debe ingresar una contraseña')
-]
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const db = require('../../database/models');
 
-module.exports = singInValidation
+const singInValidation = [
+    body('email').notEmpty().withMessage('El email es obligatorio').isEmail().withMessage('El email no tiene un formato válido'),
+    body('password').notEmpty().withMessage('La contraseña es obligatoria')
+];
+
+const processLogin = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.render('./users/login', {
+            errors: errors.mapped(),
+            old: req.body,
+            css: '/css/forms.css'
+        });
+    }
+
+    try {
+        const user = await db.Users.findOne({ where: { email: req.body.email } });
+
+        if (!user) {
+            return res.render('./users/login', {
+                errors: {
+                    email: {
+                        msg: 'Usuario no encontrado'
+                    }
+                },
+                old: req.body,
+                css: '/css/forms.css'
+            });
+        }
+
+        const passwordMatch =  await bcrypt.compare(req.body.password, user.password);
+
+        if (!passwordMatch) {
+            return res.render('./users/login', {
+                errors: {
+                    password: {
+                        msg: 'Contraseña incorrecta'
+                    }
+                },
+                old: req.body,
+                css: '/css/forms.css'
+            });
+        }
+
+        req.session.user = user; 
+        return res.redirect('/'); 
+    } catch (error) {
+        console.error('Error al procesar el login:', error);
+        return res.status(500).send('Error interno del servidor');
+    }
+};
+
+module.exports = {
+    singInValidation,
+    processLogin
+};
