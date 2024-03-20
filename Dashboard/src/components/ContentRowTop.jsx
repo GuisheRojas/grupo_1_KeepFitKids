f5f947f15ccdefb17c5b5db5752ba10ad11b1960
AnitaMigueles <anita.migueles@gmail.com>
2024-03-20 01:29:27 -0300

    resolviendo conflictos

--- Dashboard/src/components/ContentRowTop.jsx
+++ Dashboard/src/components/ContentRowTop.jsx
@@ -1,48 +1,62 @@
 import ContentRowMovies from "./ContentRowMovies"
 import GenresInDb from "./GenresInDb"
 import LastMovieInDb from "./LastMovieInDb"
-//import mandalorian from '../assets/images/mandalorian.jpg'
+import { Component } from "react";
 
 
-const ContentRowTop = () => {
-  const contentRowMovies = [
-    { titulo: "Movies in Data Base", cifra: 21, colorBorde: "primary", icono: "film" },
-    { titulo: "Total awards", cifra: 79, colorBorde: "success", icono: "award" },
-    { titulo: "Actors quantity", cifra: 49, colorBorde: "warning",icono: "user" }
-  ]
+class ContentRowTop extends Component {
+  constructor(props){
+    super(props);{
+      this.state = {
+        totalProd: 0,
+        totalUsers: 0,
+        totalCategories: 0
+      }
+    }
+  }
 
-  const lastMovieInDB = [{
-    imagen: '../assets/images/mandalorian.jpg',
-    alt: "Star Wars - Mandalorian",
-    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?"
-  }]
+  componentDidMount(){
+    fetch('http://localhost:8000/api/products')
+      .then(response => response.json())
+      .then(data => this.setState = {
+        totalProd: data.count,
+        totalCategories: data.
+      })
+      .catch(err => console.log(err))
+  }
 
-  return (
-    <div className="container-fluid">
-      <div className="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
-        </div>
+  mostrarInfo(){
 
-        <div className="row">
-          {contentRowMovies.map((content, index) => 
-              <ContentRowMovies
-                key = {index + content}
-                titulo = {content.titulo}
-                cifra = {content.cifra}
-                colorBorde = {content.colorBorde}
-                icono = {content.icono}
-                />
-            )}
-        </div>
+  }
 
-        {/*<ContentRowMovies/>*/}
-        
-        <div className="row">
-            <LastMovieInDb img={lastMovieInDB.imagen} alt={lastMovieInDB.alt} description={lastMovieInDB.descripcion}/>
-            <GenresInDb/>
-        </div>
-    </div>
-  )
+  render(){
+    return (
+      <div className="container-fluid">
+        <div className="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+          </div>
+
+          <div className="row">
+            {contentRowMovies.map((content, index) => 
+                <ContentRowMovies
+                  key = {index + content}
+                  titulo = {content.titulo}
+                  cifra = {content.cifra}
+                  colorBorde = {content.colorBorde}
+                  icono = {content.icono}
+                  />
+              )}
+          </div>
+
+          {/*<ContentRowMovies/>*/}
+          
+          <div className="row">
+              <LastMovieInDb img={lastMovieInDB.imagen} alt={lastMovieInDB.alt} description={lastMovieInDB.descripcion}/>
+              <GenresInDb/>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default ContentRowTop
--- Dashboard/src/components/ContentWrapper.jsx
+++ Dashboard/src/components/ContentWrapper.jsx
@@ -3,15 +3,15 @@ import Footer from "./Footer"
 import Topbar from "./Topbar"
 
 const ContentWrapper = () => {
-  return (
-      <div id="content-wrapper" className="d-flex flex-column">
-          <div id="content">
-              <Topbar/>
-              <ContentRowTop/>
-          </div>
-          <Footer/>
-      </div>
-  )
+    return (
+        <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+                <Topbar/>
+                <ContentRowTop/>
+            </div>
+            <Footer/>
+        </div>
+    )
 }
 
 export default ContentWrapper
