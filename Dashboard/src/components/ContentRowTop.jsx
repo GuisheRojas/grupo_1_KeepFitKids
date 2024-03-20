a3d5da54d0d0f37121bfb03b1fb94f85960dc65b
Santiago-Rearte <santyrearte@gmail.com>
2024-03-20 17:36:58 -0300

    Haciendo funcionar el dashboard

--- Dashboard/src/components/ContentRowTop.jsx
+++ Dashboard/src/components/ContentRowTop.jsx
@@ -1,89 +1,79 @@
-import ContentRowMovies from "./ContentRowMovies"
-import GenresInDb from "./GenresInDb"
-import LastMovieInDb from "./LastMovieInDb"
-import { Component } from "react";
-
-let contentRowMovies;
-
-const lastMovieInDB = [{
-    imagen: '../assets/images/mandalorian.jpg',
-    alt: "Star Wars - Mandalorian",
-    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?"
-  }]
+import ContentRowMovies from "./ContentRowMovies";
+import GenresInDb from "./GenresInDb";
+import LastMovieInDb from "./LastMovieInDb";
+import React, { Component } from "react";
 
 class ContentRowTop extends Component {
-  constructor(props){
-    super(props);{
-      this.state = {
-        totalProd: 0,
-        totalUsers: 0,
-        totalCategories: 0
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalProd: 0,
+      totalUsers: 0,
+      totalCategories: 0
+    };
   }
 
-  componentDidMount(){
-    fetch('http://localhost:8000/api/products')
-      .then(response => response.json())
-      .then(data => {
+  componentDidMount() {
+    fetch("http://localhost:8000/api/products")
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data.count);
         this.setState({
           totalProd: data.count,
-          totalCategories: Object.keys(data.countByCategrory).length
-        })
+          totalCategories: Object.keys(data.countByCategory).length // Corrección de 'countByCategrory' a 'countByCategory'
+        });
       })
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err));
 
-      fetch('http://localhost:8000/api/users')
-      .then(response => response.json())
-      .then(data => {
+    fetch("http://localhost:8000/api/users")
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data.count);
         this.setState({
           totalUsers: data.count
-        })
+        });
       })
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err));
   }
 
-  contentRowMovies = [
-    { titulo: "Movies in Data Base", cifra: this.state.totalProd, colorBorde: "primary", icono: "film" },
-    { titulo: "Total awards", cifra: this.state.totalUsers, colorBorde: "success", icono: "award" },
-    { titulo: "Actors quantity", cifra: this.state.totalCategories, colorBorde: "warning",icono: "user" }
-  ]
+  render() {
+    const lastMovieInDB = [{
+      imagen: '../assets/images/mandalorian.jpg',
+      alt: "Star Wars - Mandalorian",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?"
+    }];
 
-  mostrarInfo(){
+    const contentRowMovies = [
+      { titulo: "Movies in Data Base", cifra: this.state.totalProd, colorBorde: "primary", icono: "film" },
+      { titulo: "Total awards", cifra: this.state.totalUsers, colorBorde: "success", icono: "award" },
+      { titulo: "Actors quantity", cifra: this.state.totalCategories, colorBorde: "warning", icono: "user" }
+    ];
 
-  }
-
-  render(){
     return (
       <div className="container-fluid">
         <div className="d-sm-flex align-items-center justify-content-between mb-4">
-              <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
-          </div>
-  
-          <div className="row">
-            
-              {contentRowMovies.map((content, index) => 
-                <ContentRowMovies
-                  key = {index + content}
-                  titulo = {content.titulo}
-                  cifra = {this.state.totalProd}
-                  colorBorde = {content.colorBorde}
-                  icono = {content.icono}
-                  />
-              )}
-          </div>
-  
-          {/*<ContentRowMovies/>*/}
-          
-          <div className="row">
-              <LastMovieInDb img={lastMovieInDB.imagen} alt={lastMovieInDB.alt} description={lastMovieInDB.descripcion}/>
-              <GenresInDb/>
-          </div>
+          <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+        </div>
+
+        <div className="row">
+          {contentRowMovies.map((content, index) => (
+            <ContentRowMovies
+              key={index + content}
+              titulo={content.titulo}
+              cifra={content.cifra}
+              colorBorde={content.colorBorde}
+              icono={content.icono}
+            />
+          ))}
+        </div>
+
+        <div className="row">
+          <LastMovieInDb img={lastMovieInDB[0].imagen} alt={lastMovieInDB[0].alt} description={lastMovieInDB[0].descripcion} />
+          <GenresInDb />
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default ContentRowTop
+export default ContentRowTop;
