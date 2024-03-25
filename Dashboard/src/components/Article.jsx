4f22e99c7b617d6e586c6b1c2a1a9ba8e471b8c4
AnitaMigueles <anita.migueles@gmail.com>
2024-03-25 18:04:38 -0300

    Mejoré la estructura del article

--- Dashboard/src/components/Article.jsx
+++ Dashboard/src/components/Article.jsx
@@ -1,12 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Article = ({id, name, description, price, category, is_new, image}) => {
     return(
-        <div>
-            <div className="card-body">
-                <img src={image} alt={name} className="product_img"/>
-                <p className="price">${price}</p>
-                <h4 className="description">{description}</h4>
+        
+            <div className="card shadow mb-4">
+                <div className="card-body">
+                <Link to={`/products/detail/${id}`}>
+                    <div className="text-center">
+                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4 product_img" style={{ width: "40rem" }} src={image} alt={name}/>
+                    </div>
+                </Link>
+                    <p className="price">${price}</p>
+                    <h4 className="description">{description}</h4>
+                </div>
             </div>
-        </div>
+        
         
     )
 }
